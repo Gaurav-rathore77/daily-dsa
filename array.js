a5953d1359d6ecd1c292986e17dad889e19aba8d
laravelrytclick-gif <laravel.rytclick@gmail.com>
2026-04-07 13:16:29 +0530

    add-findduplicate

--- array.js
+++ array.js
@@ -32,13 +32,15 @@ function moveZeroes(nums){
 
 }
 
-function findDuplicate(nums){
-    for(let i = 0 ; i <nums.length ; i ++){
-        let index = Math.abs(nums[i]) - 1
-        if(nums[index] < 0){
-            return Math.abs(nums[i])
+function findDuplicate(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        let index = Math.abs(nums[i]) - 1;
+
+        if (nums[index] < 0) {
+            return Math.abs(nums[i]);
         }
-        nums[index] = -nums[index]
+
+        nums[index] = -nums[index];
     }
 }
 
